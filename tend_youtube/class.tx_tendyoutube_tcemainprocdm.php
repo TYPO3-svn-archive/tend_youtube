0544cf1d7817f5221995219fbaa4edbe469900dd
otobrglez <otobrglez@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-02-23 09:06:12 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tend_youtube/trunk@30347 735d13b6-9817-0410-8766-e36946ffe9aa

--- tend_youtube/class.tx_tendyoutube_tcemainprocdm.php
+++ tend_youtube/class.tx_tendyoutube_tcemainprocdm.php
@@ -5,19 +5,7 @@
 
 class tx_tendyoutube_tcemainprocdm {
 
-    /* Pred shranjevanjem v bazo */
     function processDatamap_preProcessFieldArray($incomingFieldArray, $table, $id, &$ref) {
-        /*
-        if($table == "tx_tendyoutube_video") {
-            $pom = $this->getRows(sprintf("SELECT v.* FROM tx_tendyoutube_video v WHERE v.uid = %d",$id));
-            if(count($pom) == 0) return;
-
-            $tyt = new tx_tendyoutube();
-            $tyt->setVideo($pom[0]);
-            $tyt->setNewVideo($incomingFieldArray);
-            
-            //TODO:: Ne gre to... ne vem zaka?! MRS Typo3!!! $tyt->updateVideo();
-        } */
     }
 
     /* Po shranjevanju v bazo */
